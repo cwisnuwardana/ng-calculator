bffd3a64ab53897e35c6df803a6919e753b6f607
cwisnuwardana <cahyadisutoitec@gmail.com>
2026-04-28 20:26:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,14 @@ st.caption("Developed by Cahyadi Wisnu Wardana")
 # INPUT
 st.header("Gas Composition (%)")
 ch4 = st.number_input("CH4 (Methane)", value=90.0)
-c2h6 = st.number_input("C2H6 (Ethane)", value=5.0)
+c2h6 = st.number_input("C2H6 (Ethane)", value=4.72)
 c3h8 = st.number_input("C3H8 (Propane)", value=2.0)
 c4h10 = st.number_input("C4H10 (Butane)", value=0.5)
 n2 = st.number_input("N2 (Nitrogen)", value=1.8)
-co2 = st.number_input("CO2 (Carbon Dioxed", value=0.7)
+co2 = st.number_input("CO2 (Carbon Dioxed)", value=0.7)
+c5h12 = st.number_input("C5H12 (Pentane)", value=0.16)
+c6h14 = st.number_input("C6H14 (Hexane)", value=0.12)
+st.header("Total Gas Composition (%)") = (ch4 + c2h6 + c3h8 + c4h10 + n2 + co2 + c5h12 + c6h14)*100%
 
 st.header("Process Condition")
 P = st.number_input("Pressure (bar abs)", value=7.0)
@@ -19,7 +22,7 @@ Z = st.number_input("Z factor", value=1.0)
 price = st.number_input("Gas Price (Rp/Nm3)", value=10000)
 
 # CALCULATION
-M = (ch4*16 + c2h6*30 + c3h8*44 + c4h10*58 + n2*28 + co2*44)/100
+M = (ch4*16 + c2h6*30 + c3h8*44 + c4h10*58 + n2*28 + co2*44 + c5h12*72 + c6h14*72)/100
 Rs = 8.314 / (M/1000)
 rho = P / (Rs * T * Z)
 energy = 35 * (ch4/100)
